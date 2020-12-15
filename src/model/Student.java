4ebe12dacf2fb2cdce584780d0f6249dadb20e85
simin161 <simin.ra161.2018@uns.ac.rs>
2020-12-15 18:50:09 +0100

    #student dodata klasa

---
+++ src/model/Student.java
@@ -0,0 +1,26 @@
+package model;
+
+
+import model.nabrojiviTipovi.Status;
+
+public class Student {
+	
+	private String prezime;
+	private String ime;
+	private String datumRodjenja;
+	private String adresaStanovanja;
+	private String telefon;
+	private String email;
+	private String brIndeksa;
+	private int godinaUpisa;
+	private int trenutnaGodina;
+	private Status status;
+	private double prosecnaOcena;
+	//fali lista ocena (polozenih predmeta)
+	//fali lista nepolozenih predmeta
+	
+	Student(){
+		
+	}
+
+}
