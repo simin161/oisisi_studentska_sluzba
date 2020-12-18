3a7afdde01fce03f1e851b2ad0c82a433fadaaf7
simin161 <simin.ra161.2018@uns.ac.rs>
2020-12-18 18:18:44 +0100

    #ocena konstruktor, get i set metode

--- src/model/Ocena.java
+++ src/model/Ocena.java
@@ -6,5 +6,37 @@ public class Ocena {
 	private int	ocena;
 	private String datumPolaganja;
 	
-	Ocena() {}
+	public Ocena(Student student, int ocena, String datumPolaganja) {
+		super();
+		this.student = student;
+		this.ocena = ocena;
+		this.datumPolaganja = datumPolaganja;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public int getOcena() {
+		return ocena;
+	}
+
+	public String getDatumPolaganja() {
+		return datumPolaganja;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public void setOcena(int ocena) {
+		this.ocena = ocena;
+	}
+
+	public void setDatumPolaganja(String datumPolaganja) {
+		this.datumPolaganja = datumPolaganja;
+	}
+	
+	
+
 }
