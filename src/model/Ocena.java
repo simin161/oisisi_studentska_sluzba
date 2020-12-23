7b4dda30fee7d6dc9938414d30cf1315d6e8e1fc
simin161 <simin.ra161.2018@uns.ac.rs>
2020-12-23 12:40:55 +0100

    #ocena

--- src/model/Ocena.java
+++ src/model/Ocena.java
@@ -2,19 +2,24 @@ package model;
 
 public class Ocena {
 	private Student student;
-	//fali predmet
-	private int	ocena;
+	private Predmet predmet;
+	private int ocena;
 	private String datumPolaganja;
-	
-	public Ocena(Student student, int ocena, String datumPolaganja) {
+
+	public Ocena(Student student, Predmet predmet, int ocena, String datumPolaganja) {
 		super();
 		this.student = student;
+		this.predmet = predmet;
 		this.ocena = ocena;
 		this.datumPolaganja = datumPolaganja;
 	}
 
 	public Student getStudent() {
 		return student;
+	}
+
+	public Predmet getPredmet() {
+		return predmet;
 	}
 
 	public int getOcena() {
@@ -29,6 +34,10 @@ public class Ocena {
 		this.student = student;
 	}
 
+	public void setPredmet(Predmet predmet) {
+		this.predmet = predmet;
+	}
+
 	public void setOcena(int ocena) {
 		this.ocena = ocena;
 	}
@@ -36,7 +45,5 @@ public class Ocena {
 	public void setDatumPolaganja(String datumPolaganja) {
 		this.datumPolaganja = datumPolaganja;
 	}
-	
-	
 
 }
