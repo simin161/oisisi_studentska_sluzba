84c89fec41fe17215063c2c6768dc43cfd945e35
jandric162 <jandric.ra162.2018@uns.ac.rs>
2021-01-13 03:17:08 +0100

    #serijalizacija mala izmena

--- src/controller/ActionClose.java
+++ src/controller/ActionClose.java
@@ -2,8 +2,12 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.swing.JMenuItem;
+
+import controller.serijalizacijaDeserijalizacija.Serijalizacija;
 
 
 public class ActionClose {
@@ -14,6 +18,19 @@ public class ActionClose {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					Serijalizacija.Serialization();
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				System.exit(0);
 
 			}
